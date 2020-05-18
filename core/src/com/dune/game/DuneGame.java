a111ed0ad26df3a805431b1de46b42ebd79e8a27
Polpsi <59441238+Polpsi@users.noreply.github.com>
2020-05-18 13:59:07 +0300

    HomeWork1

--- core/src/com/dune/game/DuneGame.java
+++ core/src/com/dune/game/DuneGame.java
@@ -4,16 +4,25 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class DuneGame extends ApplicationAdapter {
 
     private SpriteBatch batch;
     private Tank tank;
+    private RunningCircle circle;
 
     @Override
     public void create() {
+        // Если я правильно понял, то первоначально создаваемое поле
+        // соответствует размерам виртуального поля,
+        // которое потом масштабируется по экрану, а то хочется уйти от "магических цифр".
+        // И такую конструкцию я могу использовать для получения границ экрана.
+        Vector2 fieldSize = new Vector2(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
         batch = new SpriteBatch();
-        tank = new Tank(200, 200);
+        tank = new Tank(200, 200, fieldSize);
+        circle = new RunningCircle(700, 400, fieldSize);
     }
 
     @Override
@@ -23,22 +32,29 @@ public class DuneGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(0, 0.4f, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        tank.render(batch);
+        renderAll();
         batch.end();
     }
 
     public void update(float dt) {
+        circle.update(tank.getPosition(), tank.getSize());
         tank.update(dt);
+    }
+
+    public void renderAll() {
+        circle.render(batch);
+        tank.render(batch);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
         tank.dispose();
+        circle.dispose();
     }
 
     // Домашнее задание:
-    // - Задать координаты точки, и нарисовать в ней круг (любой круг, радиусом пикселей 50)
-    // - Если "танк" подъедет вплотную к кругу, то круг должен переместиться в случайную точку
-    // - * Нельзя давать танку заезжать за экран
+    // - Задать координаты точки, и нарисовать в ней круг (любой круг, радиусом пикселей 50) +
+    // - Если "танк" подъедет вплотную к кругу, то круг должен переместиться в случайную точку +
+    // - * Нельзя давать танку заезжать за экран +
 }
--- core/src/com/dune/game/Tank.java
+++ core/src/com/dune/game/Tank.java
@@ -7,36 +7,62 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
-    class Tank {
-        private Vector2 position;
-        private Texture texture;
-        private float angle;
-        private float speed;
+class Tank {
+    private Vector2 position;
+    private Vector2 fieldSize;
+    private Texture texture;
+    private float angle;
+    private float speed;
 
-        public Tank(float x, float y) {
-            this.position = new Vector2(x, y);
-            this.texture = new Texture("tank.png");
-            this.speed = 200.0f;
+
+    public Tank(float x, float y, Vector2 fieldSize) {
+        this.position = new Vector2(x, y);
+        this.fieldSize = fieldSize;
+        this.texture = new Texture("tank.png");
+        this.speed = 200.0f;
+    }
+
+    public void update(float dt) {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            angle += 180.0f * dt;
         }
-
-        public void update(float dt) {
-            if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-                angle += 180.0f * dt;
-            }
-            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-                angle -= 180.0f * dt;
-            }
-            if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-                position.x += speed * MathUtils.cosDeg(angle) * dt;
-                position.y += speed * MathUtils.sinDeg(angle) * dt;
-            }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            angle -= 180.0f * dt;
         }
-
-        public void render(SpriteBatch batch) {
-            batch.draw(texture, position.x - 40, position.y - 40, 40, 40, 80, 80, 1, 1, angle, 0, 0, 80, 80, false, false);
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            move(speed,angle,dt);
         }
+        // Не мог не сделать движение назад, но в два раза медленее.
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            move(speed/2,angle+180, dt);
+            }
 
-        public void dispose() {
-            texture.dispose();
+    }
+
+    private void move(float speed, float angle, float dt) {
+        float newX = position.x + speed * MathUtils.cosDeg(angle) * dt;
+        float newY = position.y + speed * MathUtils.sinDeg(angle) * dt;
+        // Если достигли края поля, то координаты останутся прежними, то есть - упёрлись.
+        if ((newX > texture.getWidth() / 2f) && (newX < fieldSize.x - texture.getWidth() / 2f) &&
+                (newY > texture.getHeight() / 2f) && (newY < fieldSize.y - texture.getHeight() / 2f)) {
+            position.x = newX;
+            position.y = newY;
         }
     }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, position.x - 40, position.y - 40, 40, 40, 80, 80, 1, 1, angle, 0, 0, 80, 80, false, false);
+    }
+
+    public void dispose() {
+        texture.dispose();
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public Vector2 getSize() {
+        return new Vector2(texture.getWidth(), texture.getHeight());
+    }
+}
